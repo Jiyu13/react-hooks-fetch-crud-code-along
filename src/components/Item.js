8dd42852504b55cc6bd00781f74c22ce5b97b2fb
Ziru Zou <jiyucoding@yahoo.com>
2023-02-06 10:07:43 -0500

    add onClick event listener on button, create function to handle button click

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 function Item({ item }) {
+
+  // 3. Update items:
+  // Add function to handle button click
+  function handleAddtoCartClick() {
+    console.log("clicked item:", item)
+  }
+
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"} onClick={handleAddtoCartClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
